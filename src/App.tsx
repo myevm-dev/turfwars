aee39ef59298f4c4dda753530e38d22bc3179420
My EVM <yo@myevm.org>
2025-07-16 11:22:43 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { ConnectButton } from "thirdweb/react";
-import thirdwebIcon from "./thirdweb.svg";
+// src/App.tsx
+import { /* ConnectButton */ } from "thirdweb/react";
 import { client } from "./client";
 
 export function App() {
 	return (
-		<main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-			<div className="py-20">
+		<main className="p-4 pb-10 min-h-screen flex items-center justify-center bg-black text-white">
+			<div className="flex flex-col items-center justify-center w-full py-20">
 				<Header />
 
-				<div className="flex justify-center mb-20">
+				{/* <div className="flex justify-center mb-20">
 					<ConnectButton
 						client={client}
 						appMetadata={{
-							name: "Example app",
-							url: "https://example.com",
+							name: "Turf Wars",
+							url: "https://turfwars.fun",
 						}}
 					/>
-				</div>
+				</div> */}
 
 				<ThirdwebResources />
 			</div>
@@ -26,52 +26,28 @@ export function App() {
 
 function Header() {
 	return (
-		<header className="flex flex-col items-center mb-20 md:mb-20">
+		<header className="flex flex-col items-center mb-12">
 			<img
-				src={thirdwebIcon}
-				alt=""
-				className="size-[150px] md:size-[150px]"
-				style={{
-					filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-				}}
+				src="/favicon.png"
+				alt="Turfwars Logo"
+				className="w-36 h-36 mb-6"
 			/>
-
-			<h1 className="text-2xl md:text-6xl font-bold tracking-tighter mb-6 text-zinc-100">
-				thirdweb SDK
-				<span className="text-zinc-300 inline-block mx-1"> + </span>
-				<span className="inline-block -skew-x-6 text-violet-500"> vite </span>
+			<h1 className="text-4xl md:text-6xl font-bold tracking-tighter mb-6 text-center text-zinc-100">
+				Turfwars
+				<span className="text-zinc-300 mx-1">.</span>
+				<span className="-skew-x-6 text-violet-500">Fun</span>
 			</h1>
-
-			<p className="text-zinc-300 text-base">
-				Read the{" "}
-				<code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-					README.md
-				</code>{" "}
-				file to get started.
-			</p>
 		</header>
 	);
 }
 
 function ThirdwebResources() {
 	return (
-		<div className="grid gap-4 lg:grid-cols-3 justify-center">
+		<div className="flex justify-center w-full">
 			<ArticleCard
-				title="thirdweb SDK Docs"
-				href="https://portal.thirdweb.com/typescript/v5"
-				description="thirdweb TypeScript SDK documentation"
-			/>
-
-			<ArticleCard
-				title="Components and Hooks"
-				href="https://portal.thirdweb.com/typescript/v5/react"
-				description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-			/>
-
-			<ArticleCard
-				title="thirdweb Dashboard"
-				href="https://thirdweb.com/dashboard"
-				description="Deploy, configure, and manage your smart contracts from the dashboard."
+				title="Trade Now"
+				href="https://www.syncfolio.space/turfwars"
+				description="Hosted By SyncFolio Space."
 			/>
 		</div>
 	);
@@ -84,14 +60,14 @@ function ArticleCard(props: {
 }) {
 	return (
 		<a
-			href={`${props.href}?utm_source=vite-template`}
+			href={`${props.href}?utm_source=turfwars`}
 			target="_blank"
-			className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
+			className="block w-full max-w-3xl border border-zinc-800 p-10 rounded-xl hover:bg-zinc-900 hover:border-zinc-700 transition-colors text-center flex flex-col items-center justify-center"
 			rel="noreferrer"
 		>
 			<article>
-				<h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-				<p className="text-sm text-zinc-400">{props.description}</p>
+				<h2 className="text-3xl font-semibold mb-2">{props.title}</h2>
+				<p className="text-lg text-zinc-400">{props.description}</p>
 			</article>
 		</a>
 	);
